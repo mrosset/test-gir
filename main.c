7345908ad72bd2936a62df1ec737832b7a778bac
Mike Rosset <mike.rosset@gmail.com>
2019-12-23 21:47:34 -0800

    main: complete C example

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <girepository.h>
+#include <glib.h>
 
 int main(void) {
   GError *error = NULL;
   GIRepository *gir = g_irepository_get_default();
-  g_irepository_require(repository, "CLutter", "1.0", 0, &error);
+  GIBaseInfo *info;
+  GIInfoType type;
+  const char *expect = "ClutterPaintNode";
+
+  g_irepository_require(gir, "Clutter", "1.0", 0, &error);
+
   if (error) {
     g_error("ERROR: %s\n", error->message);
     return 1;
   }
+  info = g_irepository_find_by_name(gir, "Clutter", "PaintNode");
+
+  if (!info) {
+    g_error("ERROR: %s\n", "Could not find Clutter PaintNode");
+    return 1;
+  }
+
+  type = g_registered_type_info_get_g_type(info);
+  if (!g_str_equal(expect, g_type_name(type))) {
+    g_error("expected %s got %s\n", expect, g_type_name(type));
+  } else {
+    g_print("PASS\n");
+  }
+
+  g_base_info_ref(info);
   return 0;
 }
